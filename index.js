1f2a77be0313dc2245271af6ef5a937b482c811e
JohannaMathieu <johannamathieu@yahoo.de>
2021-03-11 10:11:08 +0100

    changed console log

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ const firstValue = 1;
 const operator = "+";
 const secondValue = 2;
 
-console.log(firstValue);
-console.log(operator);
-console.log(secondValue);
+console.log(Number(firstValue) + Number(secondValue));
 

c785c5a53a5203711e5a6df5dfb8853a2ce411c8
JohannaMathieu <johannamathieu@yahoo.de>
2021-03-11 11:34:48 +0100

    check for math operators

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const firstValue = 1;
 const operator = "+";
 const secondValue = 2;
 
-if (operator === "+") {
-    console.log(Number(firstValue) + Number(secondValue));
+// if (operator === "+") {
+//     console.log(Number(firstValue) + Number(secondValue));
+// }
+// else {
+//     console.log(Number(firstValue) - Number(secondValue));
+// }
+
+switch (operator) {
+    case "+":
+        console.log(Number(firstValue) + Number(secondValue));
+        break;
+    case "-":
+        console.log(Number(firstValue) - Number(secondValue));
+        break;
+    case "*":
+        console.log(Number(firstValue) * Number(secondValue));
+        break;
+    case "/":
+        console.log(Number(firstValue) / Number(secondValue));
+        break;
+    default:
+        console.log("Invalid operator");
 }
-else {
-    console.log(Number(firstValue) - Number(secondValue));
-}
+
